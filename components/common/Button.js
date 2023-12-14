1cac3a708fe390cc0b5eab6fb9286db0a4cdd143
alllex123456 <alextanase454@gmail.com>
2023-12-14 12:46:41 +0200

    --

--- components/common/Button.js
+++ components/common/Button.js
@@ -13,6 +13,15 @@ const Button = ({ size, type, label, onClick, className }) => {
       ? 'text-[1.2rem] px-12 py-2'
       : 'text-[1.4rem] px-16 py-2 md:px-6 md:text-[1.4rem]';
 
+  const generateKey = (item, index) => {
+    if (typeof item === 'string') {
+      return `text-${item.toLowerCase()}-key-${index}`;
+    } else if (React.isValidElement(item) && item.type) {
+      return `component-${item.type.name || 'unknown'}-key-${index}`;
+    }
+    return `default-key-${index}`;
+  };
+
   return (
     <button
       className={`${typeClasses} ${sizeClass} ${className} transition delay-100 inline-flex items-center justify-center rounded-[2px] font-bold w-fit ${
@@ -20,18 +29,20 @@ const Button = ({ size, type, label, onClick, className }) => {
       }`}
       onClick={onClick}
     >
-      {label.map((item, index) => (
-        <span
-          key={Math.random()}
-          className={`${
-            typeof item === 'string' && size !== 'small'
-              ? 'pt-[2px]'
-              : 'pt-[4px]'
-          } ${typeof item !== 'string' ? 'pb-2' : ''}`}
-        >
-          {typeof item === 'string' ? item.toUpperCase() : item}
-        </span>
-      ))}
+      {label.map((item, index) => {
+        return (
+          <span
+            key={generateKey(item, index)}
+            className={`${
+              typeof item === 'string' && size !== 'small'
+                ? 'pt-[2px]'
+                : 'pt-[4px]'
+            } ${typeof item !== 'string' ? 'pb-2' : ''}`}
+          >
+            {typeof item === 'string' ? item.toUpperCase() : item}
+          </span>
+        );
+      })}
     </button>
   );
 };
